42ba4da61ce5cf767b740f3cd1bf1bf074259ff9
Dan Nieuwland <dan.nieuwland@rakuten.com>
2022-01-21 11:56:39 -0500

    Add UIKit+AFNetworking as a SPM target

--- Package.swift
+++ Package.swift
@@ -25,13 +25,20 @@
 
 import PackageDescription
 
-let package = Package(name: "AFNetworking",
-                      platforms: [.macOS(.v10_10),
-                                  .iOS(.v9),
-                                  .tvOS(.v9),
-                                  .watchOS(.v2)],
-                      products: [.library(name: "AFNetworking",
-                                          targets: ["AFNetworking"])],
-                      targets: [.target(name: "AFNetworking",
-                                        path: "AFNetworking",
-                                        publicHeadersPath: "")])
+let package = Package(
+    name: "AFNetworking",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(name: "AFNetworking", targets: ["AFNetworking"]),
+        .library(name: "UIKit_AFNetworking", targets: ["UIKit_AFNetworking"])
+    ],
+    targets: [
+        .target(name: "AFNetworking", path: "AFNetworking", publicHeadersPath: ""),
+        .target(name: "UIKit_AFNetworking", dependencies: ["AFNetworking"], path: "UIKit+AFNetworking", publicHeadersPath: "")
+    ]
+)
