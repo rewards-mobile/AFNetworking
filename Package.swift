1df91a3ac8cc71b006328df272a3a951627717a2
Dan Nieuwland <dan.nieuwland@rakuten.com>
2022-01-27 15:10:54 -0500

    Update buiild plaforms

--- Package.swift
+++ Package.swift
@@ -28,10 +28,10 @@ import PackageDescription
 let package = Package(
     name: "AFNetworking",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v9),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .macOS(.v10_13),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v5)
     ],
     products: [
         .library(name: "AFNetworking", targets: ["AFNetworking"]),
